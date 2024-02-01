56c4c6017e89d982fd8efbf3fb5606c83e6dfc79
Alessio Sarica <sarica.alessio@proton.me>
2024-02-01 21:02:13 +0100

    edit db index file

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./supabase.types";
 
-const supabaseUrl = 'https://mmpfhgsiriqzshkikeng.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tcGZoZ3NpcmlxenNoa2lrZW5nIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMjA2MTYxMiwiZXhwIjoyMDE3NjM3NjEyfQ.Agpkm8keCIkGLnkzVsBmUd3voRbPSbOoV5pY4pomKdM';
+const supabaseUrl = 'your supabase url';
+const supabaseKey = 'your supabase key';
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 export default supabase;
